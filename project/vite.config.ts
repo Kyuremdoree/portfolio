ff5aee1482c24d56093f0bba36da9ed0d6c8ef35
alexis.laurent <Alexis.LAURENT@etu.uca.fr>
2025-06-26 14:50:38 +0200

    deploy

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // '/' is the root path for GitHub Pages
+  base: '/portfolio/', // '/' is the root path for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
